1ce8caf358a01015a37a75d640d2dd52ea484029
Buse Duran <buseduran500@gmail.com>
2025-01-05 19:53:07 +0300

    🗃️ add Category column to Entries table via new migration.

---
+++ BudgetFlow.Infrastructure/Migrations/20250105162809_mig_myinitial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_myinitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Entries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Entries");
+        }
+    }
+}
